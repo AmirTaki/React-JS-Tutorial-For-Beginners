0933afb34953159396d5e497e3083e884ca9e022
amir <amirtaki623@gmail.com>
2025-07-08 22:52:13 +0330

    Event Handling In Functional

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -4,13 +4,16 @@ import Message from './components/Messagge';
 import Profile from './components/Profile';
 import Counter from './components/Counter';
 import Resume from './components/Resume';
+import FunctionEvent from './components/FunctionEvent';
 
 function App() {
   return (
     <div className="App">
-      <Profile name = "amir" lastname = "taki" />
-
+      
       <Resume  message = "message is there"/>
+
+      <hr />
+      <FunctionEvent />
       
     </div>
   );
---
+++ myproject/src/components/FunctionEvent.js
@@ -0,0 +1,13 @@
+const FunctionEvent = () => {
+    const handleClick = () => {
+        console.log('Button is clicked')
+    }
+    return (
+        <>
+            Functional Component 
+            <button onClick={handleClick}>Click here</button>
+        </>
+    )
+}
+
+export default FunctionEvent;
