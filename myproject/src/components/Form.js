fe0e769fd87e6a4c4b8027b94c4a50315185b885
amir <amirtaki623@gmail.com>
2025-07-09 22:11:38 +0330

    Handling User Input Part 2

--- myproject/src/components/Form.js
+++ myproject/src/components/Form.js
@@ -3,22 +3,36 @@ import { Component } from "react";
 class Form extends Component {
     
     state = {
-        firstname : ""
+        firstname : "",
+        lastname : ''
     }
     handleChange = (event) => {
         this.setState({firstname : event.target.value})
         console.log(this.state.firstname)
     };
+
+    handleChangeLastName = (event) => {
+        this.setState({lastname : event.target.value})
+        console.log(this.state.lastname)
+    }
     
     render(){
         return(
             <div>Form
                 <form>
+                    <label>firstName : </label>
                     <input 
                         type = "text" 
                         value = {this.state.firstname}
                         onChange={this.handleChange}
                     ></input>
+                    <label>lastName : </label>
+                    <input
+                        type = "text"
+                        value = {this.state.lastname}
+                        onChange={this.handleChangeLastName}
+                    >
+                    </input>
                 </form>
             </div>
         )
