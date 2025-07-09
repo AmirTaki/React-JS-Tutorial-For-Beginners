19a9b26f72da2dffd567b0f0f84309f84c4c6e45
amir <amirtaki623@gmail.com>
2025-07-09 23:30:42 +0330

    Todo App In React Part 4 Inline

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from './components/Todo';
+import InlineComponent from './components/InlineComponent';
 
 function App() {
   return (
     <div className="App">
+      <InlineComponent />
       <Todo />
     </div>
   );
---
+++ myproject/src/components/InlineComponent.js
@@ -0,0 +1,14 @@
+const header = {
+    color : 'blue', 
+    fontSize : '140px',
+    textAlign : "center"
+}
+
+const InlineComponent = ()=> {
+    return (
+        <>
+            <h1 style={header}>This is an inline components</h1>
+        </>
+    )
+}
+export default InlineComponent
