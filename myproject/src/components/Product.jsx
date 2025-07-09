269c17c9cf581a64b3cd3a383a9c3756e972bb56
amir <amirtaki623@gmail.com>
2025-07-09 21:35:44 +0330

    Rendering Array

--- myproject/src/components/Product.jsx
+++ myproject/src/components/Product.jsx
@@ -1,9 +1,22 @@
 const Product = () => {
-    const products = ["Labtop", "Phone", "Modem", "Computer", "Labtop"]
-    const productList = products.map((product) => (<h3 key = {product}>{product}</h3>))
+    // PRODUCTS
+    const products = [
+        {id : 1, name : 'Labtop', price : 500},
+        {id : 2, name : 'Phone', price : 200},
+        {id : 3, name : 'Modem', price : 50},
+        {id : 4, name : 'Labtop', price : 900},
+    ]
+
+    const ProductList = products.map((product) => 
+        (<h3 key = {product.id}>{product.name} : $ {product.price}</h3>))
+    // FRIUTS 
+    const fruits = ["Apple", "Mango", "Banana"]
+    const FruitsList = fruits.map((fruit, index) => (<h3 key = {index}>{fruit}</h3>))
     return(
         <>
-            {productList}
+        {ProductList}
+        <hr />
+        {FruitsList}
         </>
     )
 }
