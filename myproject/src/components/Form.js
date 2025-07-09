2133d37a824f9896454091bee0b7e069c72eba7b
amir <amirtaki623@gmail.com>
2025-07-09 23:56:18 +0330

    Adding Form Component

---
+++ myproject/src/components/Form.js
@@ -0,0 +1,13 @@
+import style from "./style.module.css"
+const Form = () => {
+    return(
+        <div className={style.todoform}>
+            <form >
+                <input className={style.todoinput} placeholder="Add Todo Item"></input>
+                <button className={style.todobutton}>Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
