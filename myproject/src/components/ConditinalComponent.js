9e803de60c1979c4ac2abe4ed725901794f1e9da
amir <amirtaki623@gmail.com>
2025-07-09 20:52:51 +0330

    Conditional Rendering Using

--- myproject/src/components/ConditinalComponent.js
+++ myproject/src/components/ConditinalComponent.js
@@ -1,20 +1,10 @@
 import { useState } from "react"
 
 export default function ConditinalComponent (){
-    const [display, setDisplay] = useState(true)
-    if(display){    
-        return (
-            <div>
-                <h3>This is a conditional component</h3>
-            </div>
-        )
-    }
-
-    else {
-        return(
-            <div>
-                <h3>Nothing to see here</h3>
-            </div>
-        )
-    }
+    const [display, setDisplay] = useState(false)
+    let output ;
+    output =  display ? <h3>This is a conditional component</h3> : <h3>Nothing to see here</h3> 
+    return(
+        <>{output}</>
+    )
 }
