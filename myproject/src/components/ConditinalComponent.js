6f5826960d3d7b9d3abb3496649d9f904b8bf92c
amir <amirtaki623@gmail.com>
2025-07-09 20:56:49 +0330

    Ternary Conditional

--- myproject/src/components/ConditinalComponent.js
+++ myproject/src/components/ConditinalComponent.js
@@ -2,9 +2,14 @@ import { useState } from "react"
 
 export default function ConditinalComponent (){
     const [display, setDisplay] = useState(false)
-    let output ;
-    output =  display ? <h3>This is a conditional component</h3> : <h3>Nothing to see here</h3> 
-    return(
-        <>{output}</>
-    )
+    return display ? (
+        <div>
+            <h3>This is a conditional component</h3> 
+        </div>
+    ) : (
+        <div>
+            <h3>Nothing to see here</h3> 
+        </div>
+    );
+
 }
