279d38abbeeb635c9669c8614b7b66f2619d7f49
amir <amirtaki623@gmail.com>
2025-07-12 02:21:26 +0330

    base react totorial

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,10 +1,53 @@
 // import logo from './logo.svg';
-import Modal from "./components/Modal";
+import "./App.css"
 function App() {
 
   return (
     <div className="App">
-      <Modal />
+        <div> 9d51cf3   ==>     Creating Portals'</div>
+        <div>  30693af   ==>     Using Memos
+        </div><div>  1656747   ==>    Why Memos Are Required
+        </div><div>  bf6b469   ==>     Fragments Usage
+        </div><div>  f294cb5   ==>     Fragments In React
+        </div><div>  c0a7715   ==>    Delete Functionality
+        </div><div>  f656849   ==>     Resolving Key Error
+        </div><div>  39bccae   ==>    Creating IDs For Todo
+        </div><div>  916a50f   ==>    Rendering Todo Items
+        </div><div>  0b177ff   ==>    Creating A Todo Component
+        </div><div>  6e5b3cc   ==>    Adding Items To Todo List p2
+        </div><div>  a55220c   ==>    Adding Items To Todo List p1
+        </div><div>  2133d37   ==>    Adding Form Component
+        </div><div>  9871925   ==>    Adding Header'
+        </div><div>  9c96b26   ==>    Styling The App Using CSS Modules
+        </div><div>  50664b2   ==>     Styling App Using CSS Stylesheet
+        </div><div>  19a9b26   ==>    Todo App In React Part 4 Inline
+        </div><div>  7869ee3   ==>    Todo App In React Part 3
+        </div><div>  fd97009   ==>    Todo App In React Part 2
+        </div><div>  eac3adc   ==>     Todo App In React Part 1
+        </div><div>  eb8dc12   ==>     Form Submission
+        </div><div>  fe0e769   ==>     Handling User Input Part 2
+        </div><div>  00ac05f   ==>    Handling User Input Part 1
+        </div><div>  269c17c   ==>     Rendering Array
+        </div><div>  be85be5   ==>     Understanding Keys
+        </div><div>  ecde86b   ==>    List Rendering
+        </div><div>  6f58269   ==>    Ternary Conditional
+        </div><div>  9e803de   ==>    Conditional Rendering Using
+        </div><div>  909a1cf   ==>    Conditional Rendering
+        </div><div>  07757b0   ==>    Using State In A Functional
+        </div><div>  6696667   ==>    Event Binding
+        </div><div>  672b549   ==>    Event Handling In Class Based
+        </div><div>  0933afb   ==>    Event Handling In Functional
+        </div><div>  53eb04f   ==>     Destructuring Props
+        </div><div>  b3e92b3   ==>    Using State Part 3
+        </div><div>  39aedc7   ==>    Using State Part 2
+        </div><div>  6926194   ==>     Using State
+        </div><div>  ae72be9   ==>    Immutability Of Props
+        </div><div>  324050e   ==>     Props In A Class Based Component
+        </div><div>  9a1a4ff   ==>    Props In Functional Component
+        </div><div>  b9d8849   ==>    Props in Functional Component
+        </div><div>  5e18aba   ==>    Class Components In React
+        </div><div>  db4e17e   ==>     Functional Components In React
+        </div> <div> bebf753   ==>    create folders react toturial </div>
     </div>
   );
 }
